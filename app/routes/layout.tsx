306602c7ea0c82c2262db983a92b873520de95a3
JKmoises <moi.prado@duocuc.cl>
2025-03-27 13:17:48 -0300

    feat: Crear submenu para reportes de cartera

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -2,9 +2,11 @@ import { Link, Outlet, useLocation } from "react-router";
 
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "presentation/components/ui";
 import { cn } from "presentation/lib/utils";
@@ -15,17 +17,22 @@ const routes = [
     name: "Estado de cuenta",
   },
   {
-    path: "/cartera-cobrada",
-    name: "Cartera cobrada",
+    name: "Reportes de Cartera",
+    subRoutes: [
+      {
+        path: "/cartera-cobrada",
+        name: "Cartera cobrada",
+      },
+      {
+        path: "/cartera-por-cobrar",
+        name: "Cartera por cobrar",
+      },
+      {
+        path: "/antiguedad-cartera",
+        name: "Antigüedad de cartera",
+      },
+    ],
   },
-  {
-    path: "/cartera-por-cobrar",
-    name: "Cartera por cobrar",
-  },
-  {
-    path: "/antiguedad-cartera",
-    name: "Antigüedad de cartera",
-  }
 ];
 
 const MainLayout = () => {
@@ -38,22 +45,50 @@ const MainLayout = () => {
 
         <NavigationMenu>
           <NavigationMenuList>
-            {routes.map((route) => (
-              <NavigationMenuItem key={route.path}>
-                <NavigationMenuLink
-                  className={cn(
-                    navigationMenuTriggerStyle(),
-                    "text-xl rounded-none md:hover:text-slate-700",
-                    {
-                      "border-b-2 border-slate-700": pathname === route.path,
-                    }
-                  )}
-                  asChild
-                >
-                  <Link to={route.path}>{route.name}</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            ))}
+            {routes.map((route) =>
+              route.subRoutes ? (
+                <NavigationMenuItem key={route.name}>
+                  <NavigationMenuTrigger className="text-xl  rounded-none md:hover:text-slate-700 cursor-pointer">
+                    {route.name}
+                  </NavigationMenuTrigger>
+
+                  <NavigationMenuContent>
+                    <ul className="grid gap-3 py-4 px-8 md:w-[400px] lg:w-[500px] lg:grid-cols-2">
+                      {route.subRoutes.map((subRoute) => (
+                        <NavigationMenuLink
+                          key={subRoute.path}
+                          className={cn(
+                            "block px-4 py-2 text-md rounded-none md:hover:text-slate-700 text-center",
+                            {
+                              "border-b-2 border-slate-700 font-semibold":
+                                pathname === subRoute.path,
+                            }
+                          )}
+                          asChild
+                        >
+                          <Link to={subRoute.path}>{subRoute.name}</Link>
+                        </NavigationMenuLink>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              ) : (
+                <NavigationMenuItem key={route.path}>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      "text-xl rounded-none md:hover:text-slate-700",
+                      {
+                        "border-b-2 border-slate-700": pathname === route.path,
+                      }
+                    )}
+                    asChild
+                  >
+                    <Link to={route.path}>{route.name}</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              )
+            )}
           </NavigationMenuList>
         </NavigationMenu>
       </section>
